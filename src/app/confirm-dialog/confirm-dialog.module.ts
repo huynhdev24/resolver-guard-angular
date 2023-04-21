a007cf7fa389bd826f7b7e220d04eb50cce32046
huynhdev24 <huynh.dev.24@gmail.com>
2023-04-21 11:50:09 +0700

    Add article routing and article module

--- src/app/confirm-dialog/confirm-dialog.module.ts
+++ src/app/confirm-dialog/confirm-dialog.module.ts
@@ -1,23 +1,14 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ConfirmDialogComponent } from "./confirm-dialog.component";
+import {MatButtonModule} from '@angular/material/button';
 
-export interface DialogData {
-    title: string;
-}
-
-@Component({
-    selector: 'app-confirm-dialog',
-    templateUrl: './confirm-dialog.component.html',
-    styleUrls: ['./confirm-dialog.component.css']
+@NgModule({
+    imports: [
+        CommonModule,
+        MatButtonModule,
+    ],
+    declarations: [ConfirmDialogComponent],
+    exports: [ConfirmDialogComponent]
 })
-export class ConfirmDialogComponent implements OnInit {
-    constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
-
-    ngOnInit(): void {
-    }
-
-    doAction(action: boolean) {
-        this.dialogRef.close(action);
-    }
-}
+export class ConfirmDialogModule { }
